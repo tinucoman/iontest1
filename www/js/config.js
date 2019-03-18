2fa24fe8355aaab9806f932bc6e2c177414b45a6
tinu coman <tinu@medeeaweb.com>
2019-03-18 23:54:24 +0200

    be deploy

--- www/js/config.js
+++ www/js/config.js
@@ -15,7 +15,7 @@ angular
     });
 */
 
-
+/*
 // Asogem NL
 angular
     .module('asogem')
@@ -29,12 +29,12 @@ angular
         updateUrl: 'https://asogem.be/repairapk/nl/production/'
         //apiUrl: 'http://repair.asogem.nl/staging/'
     });
-
+*/
 
 
 
 // Asogem BE
-/*
+
 angular
     .module('asogem')
     .constant('appConfig', {
@@ -43,8 +43,7 @@ angular
         lang_id: '1',
         version: '120',
         versionName: '1.2.0',
-        //apiUrl: 'http://repair.asogem.be/'
-        apiUrl: 'http://repair.asogem.be/staging/',
+        apiUrl: 'http://repair.asogem.be/',
+        //apiUrl: 'http://repair.asogem.be/staging/',
         updateUrl: 'https://asogem.be/repairapk/be/production/'
     });
-*/
